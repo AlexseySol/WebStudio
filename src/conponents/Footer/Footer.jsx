2f319813bfe31f72d272e00ff79d81198ae6258d
Алексей Соляной <alexsey.solyanoy@gmail.com>
2023-02-21 20:58:47 +0200

    sp

--- src/conponents/Footer/Footer.jsx
+++ src/conponents/Footer/Footer.jsx
@@ -6,46 +6,64 @@ import { ReactComponent as FacebookF } from "./svg/facebook_footer.svg";
 import { ReactComponent as LinkedinF } from "./svg/linkedin_footer.svg";
 import { ReactComponent as Btn } from "./svg/btn.svg";
 
-
-
 export const Footer = () => {
   return (
     <div className="footer_wrapper">
-      <div className="footer_title">
-        Web<span>Studio</span>
+      <div className="text_aling">
+        <div className="footer_title">
+          Web<span>Studio</span>
+        </div>
+        <div className="footer_map">м. Київ, пр-т Лесі Українки, 26</div>
+        <ul className="footer_contacts">
+          <li>
+            <a href="mailto:info@devstudio.com">info@devstudio.com</a>
+          </li>
+          <li>
+            <a href="tel:+38 096 111 11 11">+38 096 111 11 11</a>
+          </li>
+        </ul>
       </div>
-      <div className="footer_map">м. Київ, пр-т Лесі Українки, 26</div>
-      <ul className="footer_contacts">
-        <li>
-          <a href="mailto:info@devstudio.com">info@devstudio.com</a>
-        </li>
-        <li>
-          <a href="tel:+38 096 111 11 11">+38 096 111 11 11</a>
-        </li>
-      </ul>
-      <h4>приєднуйтесь</h4>
-      <ul className="footer_social">
-        <li>
-          <a href="https://www.instagram.com/"> <InstagramF /> </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/?lang=ua"> <TwitterF /> </a>
-        </li>
-        <li>
-          <a href="https://uk-ua.facebook.com/"> <FacebookF /> </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/"><LinkedinF /></a>
-        </li>
-      </ul>
-      <h4>Підпишіться на розсилку</h4>
-      <div className="footer_wrapper_input">
-      <input  type="text"  placeholder="E-mail"/>
+      <div className="text_aling">
+        <h4>приєднуйтесь</h4>
+        <ul className="footer_social">
+          <li>
+            <a href="https://www.instagram.com/">
+              {" "}
+              <InstagramF />{" "}
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/?lang=ua">
+              {" "}
+              <TwitterF />{" "}
+            </a>
+          </li>
+          <li>
+            <a href="https://uk-ua.facebook.com/">
+              {" "}
+              <FacebookF />{" "}
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/">
+              <LinkedinF />
+            </a>
+          </li>
+        </ul>
       </div>
-      <div className="footer_btn" >
-      <button >Підписатися <Btn className="btn_last" /></button>
+      <div>
+        <h4>Підпишіться на розсилку</h4>
+        <div className="block_email">
+          <div className="footer_wrapper_input">
+            <input type="text" placeholder="E-mail" />
+          </div>
+          <div className="footer_btn">
+            <button>
+              Підписатися <Btn className="btn_last" />
+            </button>
+          </div>
+        </div>
       </div>
-      
     </div>
   );
 };
